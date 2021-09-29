2b798dc499c939b3112f9d4b65adfac6c2b3ea3c
dh_lim_ <wawa4989@naver.com>
2021-09-29 18:08:09 +0900

    스프링 시큐리티

--- src/main/java/com/dohyeong/controller/MemberController.java
+++ src/main/java/com/dohyeong/controller/MemberController.java
@@ -1,23 +1,38 @@
 package com.dohyeong.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dohyeong.service.MemberService;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 @RequestMapping("/member/*")
+@Controller
 public class MemberController {
 
 	private MemberService service;
 	
-	//회원가입
-	@RequestMapping("./joinForm")
-	public String joinForm() throws Exception {
-		return "memeber/joinForm";
+	//all:누구나 접근가능  	member: 로그인해야 접근가능 	admin:로그인+관리자권한
+	@GetMapping("/all")
+	public void doAll() {
+		
+		log.info("do all can access everybody");
 	}
 	
+	@GetMapping("/member")
+	public void doMember() {
+		
+		log.info("loginde member");
+	}
 	
-	//로그인
-	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		
+		log.info("admin only");
+	}
 
 	
 }

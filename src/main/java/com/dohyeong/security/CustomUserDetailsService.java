3902f8dacfc393aeb86db8f25ccd4997f1b50e99
dh_lim_ <wawa4989@naver.com>
2021-10-08 19:03:42 +0900

    customUserDetailsService구성

---
+++ src/main/java/com/dohyeong/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.dohyeong.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.dohyeong.mapper.MemberMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Setter(onMethod_ = {@Autowired})
+	private MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		log.warn("Load User By UserName : " + userName);
+		
+		return null;
+	}
+}
